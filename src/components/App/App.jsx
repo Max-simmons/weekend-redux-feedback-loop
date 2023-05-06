6f5cdb6f34bfd75ed0671eb722ef3fecab44abfb
Max Simmons <maxsimmons20@gmail.com>
2023-05-06 13:38:07 -0500

    added route to all the pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import FeelingPage from '../FeelingPage/FeelingPage';
 
@@ -17,21 +18,33 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
     
-      
+      <Route exact path = '/'>
       <FeelingPage/>
+      </Route>
 
+      <Route exact path = '/understanding'>
       {/* <Understanding/> */}
+      </Route>
 
+      <Route exact path = '/support'>
       {/* <Support/> */}
+      </Route>
 
+      <Route exact path = '/comments'>
       {/* <Comments/> */}
+      </Route>
 
+      <Route exact path = '/review'>
       {/* <Review/> */}
+      </Route>
 
+      <Route exact path = '/thankyou'>
       {/* <ThankYou/> */}
+      </Route>
 
-
+      </Router>
     </div>
   );
 }
